447ffb6f8d84c402c41d5e11f05f9a826fd97825
freddyIng <freddy1k98@gmail.com>
2022-06-11 12:09:56 -0400

    File download done, and acl property added in the upload configuration, so now is public-read, and now can be downloaded (acces denied before)

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const router=require('express').Router();
 //configuracion de s3
-const { S3Client }=require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand, ListObjectsCommand}=require('@aws-sdk/client-s3');
 const multer=require('multer');
 const multerS3=require('multer-s3');
 const s3=new S3Client({
-  region: process.env.AWS_S3_REGION
+  region: process.env.AWS_S3_REGION,
   credentials: {
   	secretAccessKey: process.env.AWS_S3_SECRET,
     accessKeyId: process.env.AWS_S3_KEY
@@ -16,8 +16,9 @@ const upload=multer({
   storage: multerS3({
   	s3: s3,
   	bucket: process.env.AWS_S3_BUCKET,
+  	acl: 'public-read',
   	metadata: function(req, file, cb){
-  	  cb(null, {fieldName: file.fieldname})
+  	  cb(null, Object.assign({}, req.body))
   	},
   	key: function(req, file, cb){
   	  cb(null,'usuario-'+file.originalname) //Date.now().toString()
@@ -26,13 +27,27 @@ const upload=multer({
 });
 
 router.post('/upload', upload.single('user-file'), async (req, res) => {
-  console.log(3);
   console.log(req.file);
-  console.log(req.files);
 });
 
-router.get('/dowload', async (req, res) => {
-  
+router.get('/download', async (req, res) => {
+  try{
+  	//Solo envio el link del archivo en formato json
+    let fileKey=req.query.fileKey;
+    res.json({message: 'Ok', fileLink: `http://${process.env.AWS_S3_BUCKET}.s3.amazonaws.com/${fileKey}`});
+  } catch(err){
+  	res.json({message: 'error'})
+    console.log(err);
+  }
+});
+
+router.get('/objects', async (req, res) => {
+  let options={
+    Bucket: process.env.AWS_S3_BUCKET
+  };
+  let command=new ListObjectsCommand(options);
+  let result=await s3.send(command);
+  console.log(result);
 });
 
 
